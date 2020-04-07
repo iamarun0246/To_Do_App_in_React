d4c2ef7d94da5de64d30dc1029e3ddb59027e905
Arun <63234884+iamarun0246@users.noreply.github.com>
2020-04-07 17:58:35 +0530

    Updates

---
+++ precache-manifest.9e0f2cbef114d8f33c20f7694f6cb30c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2daeee425b20f4e525bada2fff6d9037",
+    "url": "/iamarun0246/To_Do_App_in_React/index.html"
+  },
+  {
+    "revision": "ea04c76db6045f11254b",
+    "url": "/iamarun0246/To_Do_App_in_React/static/css/main.48817f16.chunk.css"
+  },
+  {
+    "revision": "35ce260f3cbde8301aef",
+    "url": "/iamarun0246/To_Do_App_in_React/static/js/2.3dbd836e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iamarun0246/To_Do_App_in_React/static/js/2.3dbd836e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea04c76db6045f11254b",
+    "url": "/iamarun0246/To_Do_App_in_React/static/js/main.82d2b4a8.chunk.js"
+  },
+  {
+    "revision": "79b2ed18f79a4ff729db",
+    "url": "/iamarun0246/To_Do_App_in_React/static/js/runtime-main.27fd914d.js"
+  }
+]);
